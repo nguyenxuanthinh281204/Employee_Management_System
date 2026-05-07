d760687fbaeeed8304f3819ff25082a9dc9eba69
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-07 10:16:25 +0700

    add repositoty and dto mapper

---
+++ src/main/java/com/example/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.example.ems.mapper;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto maptoEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee maptoEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
