dd689048ab0e5e3d1f59c91f4da34cbe4957ee8b
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-07 23:33:50 +0700

    add create and getById, gatAll Employee

---
+++ src/main/java/com/example/ems/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.example.ems.controller;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+
+    //build add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    //build Get Employee REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //Build Get All Employee REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+}
---
+++ src/main/java/com/example/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.ems.service.impl;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.entity.Employee;
+import com.example.ems.exception.ResourceNotFoundException;
+import com.example.ems.mapper.EmployeeMapper;
+import com.example.ems.repository.EmployeeRepository;
+import com.example.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.maptoEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.maptoEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Empoyee is not exist with given id: "+employeeId));
+        return EmployeeMapper.maptoEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.maptoEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+}
